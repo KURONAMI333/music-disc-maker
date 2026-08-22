8bb57b705864169419b8c67dc5c1afab639625f1
KURONAMI333 <kuronami333@users.noreply.github.com>
2026-08-22 18:33:48 +0900

    feat: クリエイティブタブのアイコンをカスタムディスクにする

--- common/src/main/java/com/kuronami/musicdiscmaker/register/ModCreativeTab.java
+++ common/src/main/java/com/kuronami/musicdiscmaker/register/ModCreativeTab.java
@@ -17,7 +17,7 @@ public final class ModCreativeTab {
     public static final RegistryHolder<CreativeModeTab> MAIN =
             TABS.register("main", () -> CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)
                     .title(Component.translatable("itemGroup.music_disc_maker"))
-                    .icon(() -> new ItemStack(ModItems.MUSIC_DISC_MAKER.get()))
+                    .icon(() -> new ItemStack(ModItems.CUSTOM_MUSIC_DISC.get()))
                     .displayItems((params, output) -> {
                         output.accept(ModItems.MUSIC_DISC_MAKER.get());
                         output.accept(ModItems.GOLDEN_JUKEBOX.get());
